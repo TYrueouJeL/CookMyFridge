43b8cb75ead3d322553c9a269eeef4b521505b69
TYrueouJeL <guerinremi.pro@gmail.com>
2026-01-24 22:21:49 +0100

    Update recipe index page style

--- web/app/components/RecipeCard.vue
+++ web/app/components/RecipeCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="border border-gray-300 shadow-md hover:-translate-0.5 transition duration-300 rounded-lg p-2 pb-4 w-full">
-    <p class="mb-2 select-none">{{ recipe.name }}</p>
+  <div class="border border-gray-300 shadow-md hover:-translate-0.5 hover:shadow-md transition duration-300 rounded-lg p-2 pb-4 w-full">
+    <p class="mb-4 select-none">{{ recipe.name }}</p>
     <RouterLink
       class="border border-green-400 rounded-lg p-2 bg-green-100 hover:bg-green-300 cursor-pointer select-none transition duration-300"
       :to="`/recettes/${recipe.id}`"
