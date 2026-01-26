df2b9b08c24b1f4a5abb9f32160c23e8f6177fb8
TYrueouJeL <guerinremi.pro@gmail.com>
2026-01-26 22:49:22 +0100

    Updated CORS config

--- api/config/cors.ts
+++ api/config/cors.ts
@@ -8,7 +8,10 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: true,
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+  ],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
   exposeHeaders: [],
