189e024b6e2e9f6bf71f43d78b32c84e71dd7f93
TYrueouJeL <guerinremi.pro@gmail.com>
2026-01-27 10:35:02 +0100

    Fix recipe filter

--- web/app/pages/recettes/index.vue
+++ web/app/pages/recettes/index.vue
@@ -2,10 +2,26 @@
   <div class="p-6">
     <h1 class="text-3xl font-bold mb-6 text-center">Liste des recettes</h1>
 
+    <div class="border border-green-300 rounded-lg p-4 mb-2 max-w-md mx-auto text-center">
+      <input
+      type="text"
+      v-model="searchQuery"
+      class="border border-green-400 focus:outline-none focus:ring focus:ring-green-500 rounded-lg p-1.5 mx-1"
+      placeholder="Pizza Margherita"
+      >
+      </input>
+      <RouterLink
+      :to="`/recettes/create`"
+      class="border border-green-400 bg-green-100 hover:bg-green-300 rounded-lg p-2 mx-1"
+      >
+        Ajouter une recette
+      </RouterLink>
+    </div>
+
     <AsyncState :loading="loading" :error="error">
       <div class="columns-1 md:columns-2 lg:columns-3 gap-6 justify-items-center p-2 mx-80">
         <RecipeCard
-          v-for="recipe in recipes"
+          v-for="recipe in filteredRecipes"
           :key="recipe.id"
           :recipe="recipe"
           class="mb-6 break-inside-avoid"
@@ -16,13 +32,14 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import RecipeCard from '../../components/RecipeCard.vue'
 import AsyncState from '~/components/AsyncState.vue'
 
 const recipes = ref([])
 const loading = ref(true)
 const error = ref(null)
+const searchQuery = ref('')
 
 const fetchRecipes = async () => {
   loading.value = true
@@ -39,6 +56,17 @@ const fetchRecipes = async () => {
     loading.value = false
   }
 }
+
+const filteredRecipes = computed(() => {
+  if (!searchQuery.value.trim()) {
+    return recipes.value
+  }
+  
+  const query = searchQuery.value.toLowerCase()
+  return recipes.value.filter(recipe => 
+    recipe.name.toLowerCase().includes(query)
+  )
+})
 
 onMounted(() => {
   fetchRecipes()
