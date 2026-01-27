790f322ceedf2ae6a12e9705279465ae3d3d4b28
TYrueouJeL <guerinremi.pro@gmail.com>
2026-01-27 21:32:57 +0100

    Fixed recipe seeders

--- api/database/seeders/03_recipeSeeder.ts
+++ api/database/seeders/03_recipeSeeder.ts
@@ -1,10 +1,16 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import { RecipeSeederFactory } from '../../app/factories/recipeSeederFactory.js'
 import db from '@adonisjs/lucid/services/db'
+import User from '#models/user'
 
 export default class RecipeSeeder extends BaseSeeder {
   async run() {
     await db.from('recipes').delete()
-    await RecipeSeederFactory.createMany(10)
+    
+    const users = await User.query()
+    
+    for (const user of users) {
+      await RecipeSeederFactory.createMany(5, user.id)
+    }
   }
 }
